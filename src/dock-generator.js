bf6f1097dfe9d567db5d9c897495454407d642bd
Pat Needham <pat@needham.io>
2022-10-13 08:22:50 -0400

    edit out problemic imports

--- src/dock-generator.js
+++ src/dock-generator.js
@@ -1,10 +1,10 @@
-import fetch from 'node-fetch';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
+// import fetch from 'node-fetch';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import fs from 'fs';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 export function transformDockElement(original) {
 	const { geometry, properties } = original;
@@ -33,12 +33,12 @@ export function transformDockElement(original) {
 	};
 }
 
-async function run() {
-  const response = await fetch('https://layer.bicyclesharing.net/map/v1/nyc/stations');
-  const data = await response.json();
-  const docks = data.features.filter(f => f.type === 'Feature').map(transformDockElement);
-  var json = JSON.stringify(docks);
-  fs.writeFileSync(__dirname + '/../data/docks.json', json, 'utf8');
-}
+// async function run() {
+//   const response = await fetch('https://layer.bicyclesharing.net/map/v1/nyc/stations');
+//   const data = await response.json();
+//   const docks = data.features.filter(f => f.type === 'Feature').map(transformDockElement);
+//   var json = JSON.stringify(docks);
+//   fs.writeFileSync(__dirname + '/../data/docks.json', json, 'utf8');
+// }
 
-run();
+// run();
